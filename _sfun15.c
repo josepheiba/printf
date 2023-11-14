fd6a739170d5de5d1b63ac648161b4f01c7450b4
Badrnej <nejaa.badr@gmail.com>
2023-11-13 16:32:46 -0800

    19B

--- _sfun15.c
+++ _sfun15.c
@@ -10,12 +10,10 @@ int print_rev_string(va_list arg_ptr)
 {
 	char *buffer_1_byte;
 	int i, j;
-	char *nl = "(null)";
 
 	buffer_1_byte = va_arg(arg_ptr, char *);
-
 	if (buffer_1_byte == NULL)
-		buffer_1_byte = nl;
+		return (-1);
 
 	i = 0;
 
@@ -23,7 +21,8 @@ int print_rev_string(va_list arg_ptr)
 	{
 		i++;
 	}
-	j = i;
+
+	j = i - 1;
 	while (j >= 0)
 	{
 		write(1, buffer_1_byte + j, 1);
